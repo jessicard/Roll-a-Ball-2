48ce3c26a628235c141cfe6e4baf28d668627a39
jessicard <jessicard@github.com>
2017-03-17 23:56:27 -0700

    Comment out bad script code, try using an input

--- Assets/Scripts/Prompt.cs
+++ Assets/Scripts/Prompt.cs
@@ -5,37 +5,49 @@ using UnityEngine;
 
 public class Prompt : MonoBehaviour {
 	public float letterPause = 0.2f;
-
 	public Text textComponent;
+	public InputField textField;
 
-	// Use this for initialization
 	void Start () {
-		textComponent = GetComponent<Text>();
 		textComponent.text = "";
+		textField.ActivateInputField();
+
+//		textField.onValueChanged.AddListener (delegate {ValueChangeCheck ();});
+
 		List<PromptMessage> scriptMessages = new List<PromptMessage> ();
 
 		scriptMessages.Add (new PromptMessage (PromptMessage.MessageType.Computer, "Testing"));
 		scriptMessages.Add (new PromptMessage (PromptMessage.MessageType.User));
 		scriptMessages.Add (new PromptMessage (PromptMessage.MessageType.Hijack, "Testing2222222"));
 
-		foreach (PromptMessage message in scriptMessages) {
-			switch (message.messageType) {
-			case PromptMessage.MessageType.Computer:
-				StartCoroutine (TypeText (message.messageContent));
-				break;
-			case PromptMessage.MessageType.User:
-				break;
-			case PromptMessage.MessageType.Hijack:	
-				break;
-			}
-		}
+//		foreach (PromptMessage message in scriptMessages) {
+//			switch (message.messageType) {
+//			case PromptMessage.MessageType.Computer:
+//				StartCoroutine (TypeText (message.messageContent));
+//				break;
+//			case PromptMessage.MessageType.User:
+//				textComponent.text = textComponent.text + "\n";
+//				if (Input.GetKeyDown (KeyCode.Return)) {
+//				} else {
+//					textTransform.GetComponent<Text>().text = textTransform.GetComponent<Text>().text + Input.inputString;
+//				}
+//				break;
+//			case PromptMessage.MessageType.Hijack:	
+//				break;
+//			}
+//		}
 	}
 
-	IEnumerator TypeText (string messageContent) {
-		foreach (char letter in messageContent.ToCharArray()) {
-			textComponent.text += letter;
+//	void ValueChangeCheck () {
+//		textField.text = textField.text + "1";
+//	}
+		
 
-			yield return new WaitForSeconds (letterPause);
-		}
-	}
+//	IEnumerator TypeText (string messageContent) {
+//		foreach (char letter in messageContent.ToCharArray()) {
+//			textTransform.GetComponent<Text>().text += letter;
+//
+//			yield return new WaitForSeconds (letterPause);
+//		}
+//	}
 }
